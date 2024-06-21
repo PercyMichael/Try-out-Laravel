a897290dce6caa341891afbff608d5699e394e9c
Percy Michael <percymichael0@gmail.com>
2024-06-21 16:06:22 +0300

    first

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+<x-slot:heading>About</x-slot:heading>
+    <h1>Hi About</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+<x-slot:heading>Contact</x-slot:heading>
+    <h1>Hi Contact</h1>
+</x-layout>
